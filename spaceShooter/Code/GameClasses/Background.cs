790851d51798d565cd31bd5e44f64bd907bfd4d6
enra64 <sujomear@arcor.de>
2015-01-23 20:36:53 +0100

    background

---
+++ spaceShooter/Code/GameClasses/Background.cs
@@ -0,0 +1,25 @@
+﻿using SFML.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spaceShooter.Code.GameClasses {
+    class Background {
+        private List<Sprite> starList = new List<Sprite>();
+        private RenderTexture background;
+
+        public Background() {
+            background = new RenderTexture(Controller.Window.Size.X, Controller.Window.Size.Y);
+        }
+
+        public void update(){
+
+        }
+
+        //idea: draw onto a rendertexture, draw that behind everything
+        public void draw(){
+        }
+    }
+}
