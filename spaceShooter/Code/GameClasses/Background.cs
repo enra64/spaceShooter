eab8f153c1d30961b46089748acf3f83797c7683
enra64 <sujomear@arcor.de>
2015-01-26 00:39:07 +0100

    increased bg sprite count, testing on laptop b/c 16% gpu usage :D

--- spaceShooter/Code/GameClasses/Background.cs
+++ spaceShooter/Code/GameClasses/Background.cs
@@ -12,31 +12,36 @@ namespace spaceShooter.Code.GameClasses {
         private Random r = new Random();
         private Vector2u textureSize;
 
-        private RenderTexture test = new RenderTexture(16384, 16384);
-        private Sprite testSprite;
+        private RenderTexture background = new RenderTexture(16384, 16384);
+        private Sprite[] bgSprite = new Sprite[4];
 
         public Background() {
             textureSize = Globals.starTexture.Size;
-            for (int i = 0; i < 10000; i++) {
+            for (int i = 0; i < 100000; i++) {
                 starList.Add(new Sprite(Globals.starTexture));
                 float nextScale = (float)r.Next(10) / 15f;
                 starList[i].Scale = new Vector2f(nextScale, nextScale);
                 starList[i].Color = new Color((byte)r.Next(255), (byte)r.Next(255), (byte)r.Next(255), (byte)r.Next(255));
                 starList[i].Position = new Vector2f(r.Next(16384), r.Next(16384));
             }
-            test.Clear();
+            background.Clear();
             foreach (Sprite s in starList)
-                test.Draw(s);
-            testSprite = new Sprite(test.Texture);
+                background.Draw(s);
+            bgSprite[0] = new Sprite(background.Texture);
+            bgSprite[1] = new Sprite(background.Texture);
+            bgSprite[2] = new Sprite(background.Texture);
+            bgSprite[3] = new Sprite(background.Texture);
         }
 
         public void update(){
+
         }
 
         //idea: draw onto a rendertexture, draw that behind everything
         //if we are moving right, draw new stars on the left and delete the old ones
         public void draw(){
-            Controller.Window.Draw(testSprite);
+            foreach(Sprite s in bgSprite)
+                Controller.Window.Draw(s);
         }
     }
 }
