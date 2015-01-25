5ad576901ac5a7dbe4b2a0d3615d34defcc8707c
enra64 <sujomear@arcor.de>
2015-01-26 00:34:51 +0100

    improved background drawing speed by creating big-ass rendertexture :D

--- spaceShooter/Code/GameClasses/Background.cs
+++ spaceShooter/Code/GameClasses/Background.cs
@@ -12,7 +12,8 @@ namespace spaceShooter.Code.GameClasses {
         private Random r = new Random();
         private Vector2u textureSize;
 
-
+        private RenderTexture test = new RenderTexture(16384, 16384);
+        private Sprite testSprite;
 
         public Background() {
             textureSize = Globals.starTexture.Size;
@@ -21,8 +22,12 @@ namespace spaceShooter.Code.GameClasses {
                 float nextScale = (float)r.Next(10) / 15f;
                 starList[i].Scale = new Vector2f(nextScale, nextScale);
                 starList[i].Color = new Color((byte)r.Next(255), (byte)r.Next(255), (byte)r.Next(255), (byte)r.Next(255));
-                starList[i].Position = new Vector2f(r.Next(30000), r.Next(20000));
+                starList[i].Position = new Vector2f(r.Next(16384), r.Next(16384));
             }
+            test.Clear();
+            foreach (Sprite s in starList)
+                test.Draw(s);
+            testSprite = new Sprite(test.Texture);
         }
 
         public void update(){
@@ -31,8 +36,7 @@ namespace spaceShooter.Code.GameClasses {
         //idea: draw onto a rendertexture, draw that behind everything
         //if we are moving right, draw new stars on the left and delete the old ones
         public void draw(){
-            foreach(Sprite s in starList)
-                Controller.Window.Draw(s);
+            Controller.Window.Draw(testSprite);
         }
     }
 }
