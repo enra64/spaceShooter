c49f8d16b8e6dbc851f421a48a5e41c3da4a04e3
enra64 <sujomear@arcor.de>
2015-01-27 22:24:46 +0100

    ...bg working holy moly

--- spaceShooter/Code/GameClasses/Background.cs
+++ spaceShooter/Code/GameClasses/Background.cs
@@ -64,54 +64,36 @@ namespace spaceShooter.Code.GameClasses {
 
         public void update() {
             View calcView = Controller.Window.GetView();
-            FloatRect calcPort = new FloatRect(calcView.Center.X - calcView.Size.X / 2, calcView.Center.Y - calcView.Size.Y / 2, calcView.Size.X, calcView.Size.Y);
-            FloatRect overlap;
+            FloatRect calcPort = new FloatRect(calcView.Center.X - 1366 /*calcView.Size.X*/ / 2, calcView.Center.Y - 768 /*calcView.Size.Y*/ / 2, 1366, 768);
+            FloatRect[] outOverlaps = new FloatRect[9];
+
             //move the sprite if the view is moving off of it
-            calcPort.Intersects(bgRects[4], out overlap);
+            for (int i = 0; i < bgRects.Length; i++)
+                calcPort.Intersects(bgRects[i], out outOverlaps[i]);
+
+            Console.WriteLine(outOverlaps[4]);
+
+            if (outOverlaps[4].Width == calcPort.Width && outOverlaps[4].Height == calcPort.Height) {
+                bgMoved[0] = false;
+                bgMoved[1] = false;
+                bgMoved[2] = false;
+                bgMoved[3] = false;
+            }
+
+            if (outOverlaps[4].Left <= bgRects[4].Left && !bgMoved[0])
+                moveBackground(moveDirection.left);
+
+            if (outOverlaps[4].Top <= bgRects[4].Top && !bgMoved[3])
+                moveBackground(moveDirection.up);
+
+
+            if (outOverlaps[4].Left + calcPort.Width > bgRects[4].Left + bgRects[4].Width && !bgMoved[2])
+                moveBackground(moveDirection.right);
+
+            if (outOverlaps[4].Top + calcPort.Height > bgRects[4].Top + bgRects[4].Height && !bgMoved[1])
+                moveBackground(moveDirection.down);
             
-            //Console.WriteLine(overlap);
-
-            //totally legit
-            if (overlap.Left <= 0) {
-                //if we just jumped right, do not move
-                if (bgMoved[0] == false && bgMoved[0] == false)
-                    moveBackground(moveDirection.left);
-                //except if full intersection with main tile is reached
-                else if (overlap.Width == Controller.Window.Size.X){
-                    moveBackground(moveDirection.left);
-                    bgMoved[0] = false;
-                }
-            }
-            calcPort.Intersects(bgRects[4], out overlap);
-            if (overlap.Left >= bgRenderTexture.Size.X - Controller.Window.Size.X) {
-                if (bgMoved[2] == false && bgMoved[2] == false)
-                    moveBackground(moveDirection.right);
-                else if (overlap.Width == Controller.Window.Size.X) {
-                    moveBackground(moveDirection.right);
-                    bgMoved[2] = false;
-                }
-            }
-
-            calcPort.Intersects(bgRects[4], out overlap);
-            if (overlap.Top <= 0) {
-                if (bgMoved[3] == false && bgMoved[1] == false)
-                    moveBackground(moveDirection.up);
-                else if (overlap.Width == Controller.Window.Size.Y) {
-                    moveBackground(moveDirection.up);
-                    bgMoved[3] = false;
-                }
-            }
-            calcPort.Intersects(bgRects[4], out overlap);
-            if (overlap.Top >= bgRenderTexture.Size.Y - Controller.Window.Size.Y) {
-                if (bgMoved[1] == false && bgMoved[3] == false)
-                    moveBackground(moveDirection.down);
-                else if (overlap.Width == Controller.Window.Size.Y) {
-                    moveBackground(moveDirection.down);
-                    bgMoved[1] = false;
-                }
-            }
-            calcPort.Intersects(bgRects[4], out overlap);
-
+            
             //draw only if it would actually be seen
             for (int i = 0; i < bgRects.Length; i++) {
                 if (calcPort.Intersects(bgRects[i]))
@@ -156,8 +138,8 @@ namespace spaceShooter.Code.GameClasses {
 
         internal void draw() {
             for (int i = 0; i < bgRects.Length; i++)
-                //if(bgDraw[i])
-                Controller.Window.Draw(testShapes[i]);
+                if(bgDraw[i])
+                    Controller.Window.Draw(bgSprites[i]);
         }
     }
 }
