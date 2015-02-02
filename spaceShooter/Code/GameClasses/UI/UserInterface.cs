57d0921066ef892957d14eeee10721fc2d0aaaa3
enra64 <sujomear@arcor.de>
2015-02-02 21:15:19 +0100

    -.-

---
+++ spaceShooter/Code/GameClasses/UI/UserInterface.cs
@@ -0,0 +1,33 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+using spaceShooter.Code.GameClasses.UI;
+using spaceShooter.Code.Gamestates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spaceShooter.Code.GameClasses {
+    class UserInterface {
+        private View uiView;
+        private Game reference;
+        private Vector2f size;
+        private ProtoUIObject mainContainer;
+
+        public UserInterface(Vector2f _size, Game _ref) {
+            size = _size;
+            reference = _ref;
+            mainContainer = new Container(_size, _ref, "main_container");
+        }
+
+        public void update() {
+            mainContainer.update();
+        }
+
+        public void draw() {
+            Controller.Window.SetView(uiView);
+            mainContainer.draw();
+        }
+    }
+}
